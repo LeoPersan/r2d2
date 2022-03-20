55a2eb853b132212497ab8a9227d8b04fbed769c
Leonardo Persan <leopso1990@gmail.com>
2022-03-20 17:37:01 -0300

    fix: enderecos do autoload, routes, base_path e bin

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,20 +2,14 @@
 
 namespace Leopersan\R2d2;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
-
     protected function commands()
     {
         $this->load(__DIR__.'/commands');
 
-        require base_path('routes.php');
+        require __DIR__.'/routes.php';
     }
 }
