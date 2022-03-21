f9b5a8ae4be6d1bdc247161e84d1dfa5c24fdae1
Leonardo Persan <leopso1990@gmail.com>
2022-03-20 22:11:21 -0300

    feat: adicionando make:migration

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,12 +2,22 @@
 
 namespace Leopersan\R2d2;
 
+use Illuminate\Console\Application as Artisan;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Leopersan\R2d2\Migration\MigrateMakeCommand;
+use Leopersan\R2d2\Migration\MigrationCreator;
 
 class Kernel extends ConsoleKernel
 {
     protected function commands()
     {
+        Artisan::starting(fn ($artisan) => 
+            $artisan->add(new MigrateMakeCommand(
+                new MigrationCreator($this->app['files'], __DIR__.'/Migration/stubs'),
+                $this->app['composer']
+            ))
+        );
+
         $this->load(__DIR__.'/Commands');
 
         require __DIR__.'/routes.php';
