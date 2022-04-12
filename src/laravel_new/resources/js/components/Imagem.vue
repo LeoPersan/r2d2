63ecd984642c7975be729ada3d7d63622a3a6b0a
Leonardo Persan <leopso1990@gmail.com>
2022-04-12 19:55:34 -0300

    fix: exibir imagens da model

--- src/laravel_new/resources/js/components/Imagem.vue
+++ src/laravel_new/resources/js/components/Imagem.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <Arquivo :required="required" :accept="accept" :url="url" :multiple="multiple" @change="files => imagens = files"/>
+        <Arquivo :required="required" :accept="accept" :url="url" :multiple="multiple" v-model="imagens"/>
 		<br>
         <div class="row" v-if="multiple">
             <div class="col-6 col-md-4 col-lg-3 col-xl-2 text-center" v-for="(imagem, k) in imagens" :key="k">
-                <button type="button" class="btn btn-danger deletar" @click="deleteImage(k)">
+                <button type="button" class="btn btn-danger deletar" @click="imagens.splice(k,1)">
                     <i class="fas fa-times"></i>
                 </button>
                 <img class="img-thumbnail" :src="imagem">
@@ -12,17 +12,23 @@
         </div>
         <div class="row" v-else>
             <div class="col-6">
+                <button type="button" class="btn btn-danger deletar" @click="imagens = null">
+                    <i class="fas fa-times"></i>
+                </button>
                 <img class="img-thumbnail" :src="imagens">
             </div>
         </div>
     </div>
 </template>
 <script>
-import Arquivo from './Arquivo'
 export default {
+    model: {
+        prop: 'value',
+        event: 'change'
+    },
     data() {
         return {
-            imagens: []
+            imagens: this.value || []
         }
     },
     watch: {
@@ -31,6 +37,9 @@ export default {
         }
     },
     props: {
+        value: {
+            default: false
+        },
         required: {
             type: Boolean,
             default: false
@@ -46,11 +55,6 @@ export default {
         multiple: {
             type: Boolean,
             default: false
-        }
-    },
-	methods: {
-        deleteImage(i) {
-            this.imagens.splice(i,1)
         }
     }
 }
