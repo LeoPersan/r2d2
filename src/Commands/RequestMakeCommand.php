2484e2cbf7a01b972d77ebaaab03a8f69e5017ab
Leonardo Persan <leopso1990@gmail.com>
2022-04-12 22:40:19 -0300

    feat: especificar rules em request

--- src/Commands/RequestMakeCommand.php
+++ src/Commands/RequestMakeCommand.php
@@ -2,6 +2,7 @@
 
 namespace Leopersan\R2d2\Commands;
 
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Input\InputOption;
 
 class RequestMakeCommand extends GeneratorCommand
@@ -26,6 +27,8 @@ class RequestMakeCommand extends GeneratorCommand
      * @var string
      */
     protected $type = 'Request';
+
+    protected $uses = [];
 
     /**
      * Get the stub file for the generator.
@@ -73,9 +76,15 @@ class RequestMakeCommand extends GeneratorCommand
         $fields = collect(explode(',', $this->option('fields')))->map(fn ($field) => explode(':', $field));
         $types = $fields->map(fn ($field) => $field[1] ?? 'string');
         $fields = $fields->map(fn ($field) => $field[0]);
-        $rules = collect($fields)->map(fn ($field) => "'{$field}' => 'required|string',")->join("\n\t\t\t");
+        $rules = $types->filter(fn ($type, $index) => method_exists($this, 'getRule'.Str::studly($type)))
+                        ->map(fn ($type, $index) => $this->{'getRule'.Str::studly($type)}($fields[$index]))
+                        ->join("\n\t\t\t");
 
-        return str_replace('{{ rules }}',$rules,parent::replaceClass($stub, $name));
+        return str_replace([
+            '{{ rules }}', '{{ uses }}'
+        ],[
+            $rules, implode("\n", $this->uses)
+        ],parent::replaceClass($stub, $name));
     }
 
     protected function getOptions()
@@ -83,5 +92,73 @@ class RequestMakeCommand extends GeneratorCommand
         return [
             ['fields', null, InputOption::VALUE_REQUIRED, 'The name fields separeted with comma'],
         ];
+    }
+
+    public function getRuleCnpj($field)
+    {
+        $this->uses[] = 'use App\Rules\Cnpj;';
+        return "'{$field}' => ['required', new Cnpj],";
+    }
+
+    public function getRuleCpf($field)
+    {
+        $this->uses[] = 'use App\Rules\Cpf;';
+        return "'{$field}' => ['required', new Cpf],";
+    }
+
+    public function getRuleCpfCnpj($field)
+    {
+        $this->uses[] = 'use App\Rules\CpfCnpj;';
+        return "'{$field}' => ['required', new CpfCnpj],";
+    }
+
+    public function getRulePdf($field)
+    {
+        $this->uses[] = 'use App\Rules\Pdf;';
+        return "'{$field}' => ['required', new Pdf],";
+    }
+
+    public function getRuleImagem($field)
+    {
+        $this->uses[] = 'use App\Rules\Imagem;';
+        return "'{$field}' => ['required', new Imagem],";
+    }
+
+    public function getRuleTelefone($field)
+    {
+        $this->uses[] = 'use App\Rules\Telefone;';
+        return "'{$field}' => ['required', new Telefone],";
+    }
+
+    public function getRuleEmail($field)
+    {
+        return "'{$field}' => 'required|email:rfc,dns',";
+    }
+
+    public function getRuleBoolean($field)
+    {
+        return "'{$field}' => 'required|boolean',";
+    }
+
+    public function getRuleString($field)
+    {
+        return "'{$field}' => 'required|string',";
+    }
+
+    public function getRuleInteger($field)
+    {
+        return "'{$field}' => 'required|numeric',";
+    }
+
+    public function getRuleDecimal($field)
+    {
+        return "'{$field}' => 'required|numeric',";
+    }
+
+    public function getRuleFk($field)
+    {
+        $table = Str::plural(preg_replace('/(.*)_id$/', '$1', $field));
+        $key = preg_replace('/.*_(id)$/', '$1', $field);
+        return "'{$field}' => 'required|exists:{$table},{$key}',";
     }
 }
