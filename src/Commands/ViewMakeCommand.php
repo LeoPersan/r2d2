20c4a5ec93e644df2694e45d52d646ed60db7d90
Leonardo Persan <leopso1990@gmail.com>
2022-04-20 09:16:10 -0300

    feat: formatar campos no form

--- src/Commands/ViewMakeCommand.php
+++ src/Commands/ViewMakeCommand.php
@@ -146,14 +146,26 @@ class ViewMakeCommand extends GeneratorCommand
         $fields = collect(explode(',', $this->option('fields')))->map(fn ($field) => explode(':', $field));
         $types = $fields->map(fn ($field) => $field[1] ?? 'string');
         $fields = $fields->map(fn ($field) => $field[0]);
-        $fields = collect($fields)->map(function ($field) {
-            $label = str_replace('_', ' ', ucfirst($field));
-            return  <<<FIELDS
-            <div class="form-group col-md-12">
-                                    <label>$label</label>
-                                    <input type="text" class="form-control" v-model="form.data.$field" maxlength="190" required autofocus/>
-                                </div>
-            FIELDS;
+        $methods = [
+            'string' => 'fieldString',
+            'integer' => 'fieldInteger',
+            'decimal' => 'fieldDecimal',
+            'text' => 'fieldText',
+            'email' => 'fieldEmail',
+            'boolean' => 'fieldCheck',
+            'cnpj' => 'fieldCnpj',
+            'cpf' => 'fieldCpf',
+            'cpf_cnpj' => 'fieldCpfCnpj',
+            'telefone' => 'fieldTelefone',
+            'pdf' => 'fieldPdf',
+            'imagem' => 'fieldImagem',
+        ];
+        $fields = collect($types)->map(function ($type, $index) use ($fields, $methods) {
+            $label = str_replace('_', ' ', ucfirst($fields[$index]));
+            return '<div class="form-group col-md-12">'."\n\t\t\t\t\t\t"
+            .'<label>'.$label.'</label>'."\n\t\t\t\t\t\t"
+            .$this->{$methods[$type]}($fields[$index])."\n\t\t\t\t\t"
+            .'</div>';
         })->join("\n\t\t\t\t\t");
 
         return [
@@ -161,6 +173,66 @@ class ViewMakeCommand extends GeneratorCommand
             '{{ fields }}' => $fields,
             'DummyFields' => $fields,
         ];
+    }
+
+    public function fieldString(string $field): string
+    {
+        return  '<input type="text" class="form-control" v-model="form.data.'.$field.'" maxlength="190" required/>';
+    }
+
+    public function fieldEmail(string $field): string
+    {
+        return  '<input type="email" class="form-control" v-model="form.data.'.$field.'" maxlength="190" required/>';
+    }
+
+    public function fieldInteger(string $field): string
+    {
+        return  '<input type="number" class="form-control" v-model="form.data.'.$field.'" required/>';
+    }
+
+    public function fieldDecimal(string $field): string
+    {
+        return  '<input type="number" step="0.01" class="form-control" v-model="form.data.'.$field.'" required/>';
+    }
+
+    public function fieldText(string $field): string
+    {
+        return  '<tinymce api_key="{{ config(\'services.tiny.key\') }}" v-model="form.data.'.$field.'" url_upload_imagem="{{ route(\'upload_imagens\') }}"></tinymce>';
+    }
+
+    public function fieldCheck(string $field): string
+    {
+        return  '<input type="text" class="form-control" v-model="form.data.'.$field.'" maxlength="190" required/>';
+    }
+
+    public function fieldCnpj(string $field): string
+    {
+        return  '<the-mask type="tel" maks="##.###.###/####-##" class="form-control" v-model="form.data.'.$field.'" required></the-mask>';
+    }
+
+    public function fieldCpf(string $field): string
+    {
+        return  '<the-mask type="tel" maks="###.###.###-##" class="form-control" v-model="form.data.'.$field.'" required></the-mask>';
+    }
+
+    public function fieldCpfCnpj(string $field): string
+    {
+        return  '<the-mask type="tel" :maks="[\'###.###.###-##\', \'##.###.###/####-##\']" class="form-control" v-model="form.data.'.$field.'" required></the-mask>';
+    }
+
+    public function fieldTelefone(string $field): string
+    {
+        return  '<the-mask type="tel" maks="[\'(##) ####-####\', \'(##) #####-####\']" class="form-control" v-model="form.data.'.$field.'" required></the-mask>';
+    }
+
+    public function fieldPdf(string $field): string
+    {
+        return  '<Arquivo accept="application/pdf" v-model="form.data.'.$field.'" :required="true"></Arquivo>';
+    }
+
+    public function fieldImagem(string $field): string
+    {
+        return  '<Imagem v-model="form.data.'.$field.'" :required="true"></Imagem>';
     }
 
     protected function getOptions(): array
